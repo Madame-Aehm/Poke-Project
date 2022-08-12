78a5897c4d27f26f39dffc27abe8bbbb6eeec486
Emily <emilyrachelstickler@gmail.com>
2022-08-12 15:20:38 +0200

    displaying sprites harder than anticipated..

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ const nameSearch = document.getElementById("poke-name-search");
 const dataList = document.getElementById("all-pokemon");
 const searchButton = document.getElementById("search-button");
 const clearButton = document.getElementById("clear");
-// const randomButton = document.getElementById("show-random");
 const ddType1 = document.getElementById("type1");
 const ddType2 = document.getElementById("type2");
 const ddColour = document.getElementById("colour");
@@ -116,8 +115,8 @@ function fetchSinglePoke (pokeURL) {
     fetch(pokeURL).then(function (response) {
         return response.json()
     }).then(function (result) {
-        let singlePoke = result;
-        console.log(result);
+        const singlePoke = result;
+        console.log(singlePoke);
         createCard(singlePoke);
         return singlePoke;
     }).catch((error)=>{console.log(error)})
@@ -129,13 +128,12 @@ function createCard(singlePoke) {
     cardDiv.setAttribute("style", "display: flex; flex-direction: column; margin-bottom: 0.5em; margin-top: 0.5em; border: solid 2px black; border-radius: 6px; font-size: large; font-weight: bold;");
     const nameDiv = document.createElement("div");
     cardDiv.appendChild(nameDiv);
-    nameDiv.setAttribute("style", "overflow: hidden; ")
     nameDiv.innerHTML = singlePoke.name;
     nameDiv.setAttribute("style", "display: flex; justify-content: center; padding: 0.2em; color: white; background-color: #fc8485; align-self: stretch; border-top-left-radius: 3px 3px; border-top-right-radius: 3px 3px;")
     const img = document.createElement("img");
     img.setAttribute("style", "width: 100px; align-self:center;") 
     if (singlePoke.sprites.front_default === null) {
-        img.setAttribute("src", "assets/images/sleeping.png", "alt", singlePoke.name)
+        img.setAttribute("src", "assets/images/sleeping.png", "alt", "No image of " + singlePoke.name)
     } else {
     img.setAttribute("src", singlePoke.sprites.front_default, "alt", singlePoke.name);
     }
@@ -145,8 +143,9 @@ function createCard(singlePoke) {
     const modalBackground = document.createElement("div");
     modalBackground.setAttribute("style", "display: none; position: fixed; top: 0; left: 0; z-index: 1; height: 100%; width: 100%; background-color: rgba(0,0,0,0.2);");
     const modal = document.createElement("div");
-    modalBackground.addEventListener("click", () => modalBackground.style.display = "none");
-    modal.setAttribute("style", "display: flex; flex-direction: column; margin: 1% auto; border: solid 2px black; background-color: white; width: 80%; height: 90%; overflow: scroll;")
+    // modalBackground.addEventListener("click", () => modalBackground.style.display = "none");   //? need to specify not children of modelBackground....
+    modal.setAttribute("style", "display: flex; flex-direction: column; margin: 1% auto; border: solid 2px black; border-radius: 12px; background-color: white; width: 70%; height: 90%; overflow-y: scroll; font-size: large;")
+    
     const modalHeader = document.createElement("div");
     modalHeader.setAttribute("style", "display: flex; flex-direction: column; background-color: #fc8485;");
     const close = document.createElement("span");
@@ -158,18 +157,24 @@ function createCard(singlePoke) {
     const h1 = document.createElement("h1");
     h1.setAttribute("style", "color: white; align-self: center; margin-top: 0;")
     h1.innerHTML = singlePoke.name;
+    
     const modalContent1 = document.createElement("div");
-    modalContent1.setAttribute("style", "display: flex; flex-flow: row wrap; justify-content: space-evenly;")
+    modalContent1.setAttribute("style", "display: flex; flex-flow: row wrap; justify-content: space-around;");
+    const imgDiv = document.createElement("div");
+    imgDiv.style.width = "50%";
     const mainImg = document.createElement("img");
-    mainImg.setAttribute("src", singlePoke.sprites.other["official-artwork"].front_default, "alt", singlePoke.name);
-    mainImg.style.flexBasis = "40%"
+    if (singlePoke.sprites.other["official-artwork"].front_default === null) {
+        mainImg.setAttribute("src", "assets/images/sleeping.png", "alt", "No image of " + singlePoke.name)
+    } else {
+        mainImg.setAttribute("src", singlePoke.sprites.other["official-artwork"].front_default, "alt", singlePoke.name);
+    }
+    mainImg.style.maxWidth = "100%";
     const content1Right = document.createElement("div");
-    content1Right.setAttribute("style", "display: flex; flex-direction: column; border-left: solid 2px black; padding: 5%;");
+    content1Right.setAttribute("style", "width: 50%; display: flex; flex-direction: column; gap: 1em; padding-top: 5%");
     const typeLine = document.createElement("div");
     typeLine.setAttribute("style", "display: flex; gap: 1em;")
     const typesH3 = document.createElement("h3");
-    typesH3.setAttribute("style", "color: #dc7475; margin-top: 0; margin-bottom: 0; margin right: 1em;");
-    typesH3.innerHTML = "Types: ";
+    typesH3.innerHTML = "Type: ";
     const types = document.createElement("span");
     if (singlePoke.types[1] !== undefined) {
         types.innerHTML =
@@ -179,19 +184,106 @@ function createCard(singlePoke) {
       } else {
         types.innerHTML = singlePoke.types[0].type.name;
       }
-
-    typeLine.append(typesH3, types)
-    content1Right.append(typeLine);
     
+    const baseStatsLine = document.createElement("div");
+    const baseStatsH3 = document.createElement("h3");
+    baseStatsH3.innerHTML = "Base stats:";
+    const baseStatsTable = document.createElement("table");
+      for (let i = 0; i < singlePoke.stats.length; i++) {
+        const tr = document.createElement("tr");
+        const th = document.createElement("th");
+        th.innerHTML = singlePoke.stats[i].stat.name;
+        const td = document.createElement("td");
+        td.innerHTML = singlePoke.stats[i].base_stat;
+        tr.append(th, td);
+        baseStatsTable.append(tr);
+      }
+    const heightWeightLine = document.createElement("div");
+    heightWeightLine.setAttribute("style", "display: flex; gap: 2em;");
+    const heightSpan = document.createElement("span");
+    heightSpan.setAttribute("style", "display: flex; gap: 1em;")
+    const heightH3 = document.createElement("h3");
+    heightH3.innerHTML = "Height: ";
+    const height = document.createElement("span");
+    height.innerHTML = singlePoke.height;
+    const weightSpan = document.createElement("span");
+    weightSpan.setAttribute("style", "display: flex; gap: 1em;");
+    const weightH3 = document.createElement("h3");
+    weightH3.innerHTML = "Weight: ";
+    const weight = document.createElement("span");
+    weight.innerHTML = singlePoke.weight;
+
+    const abilitiesLine = document.createElement("div");
+    const abilitiesH3 = document.createElement("h3");
+    abilitiesH3.innerHTML = "Abilities: ";
+    const abilitiesList = document.createElement("ol");
+    for (let i = 0; i < singlePoke.abilities.length; i++) {
+        const li = document.createElement("li");
+        li.innerHTML = singlePoke.abilities[i].ability.name;
+        abilitiesList.appendChild(li);
+    }
+
+    const spriteSection = document.createElement("div");
+    spriteSection.style.padding = "1em";
+    const spriteHeader = document.createElement("div");
+    const spriteH3 = document.createElement("h3");
+    spriteH3.innerHTML = "Sprites: ";
+    const spriteFilterSection = document.createElement("span");
+    const spriteCheckbox1 = document.createElement("input");
+    spriteCheckbox1.setAttribute("type", "checkbox", "name", "sprites", "value", "front", "id", "front");
+    const spriteCheckbox1Label = document.createElement("label");
+    spriteCheckbox1Label.for = "front";
+    spriteCheckbox1Label.innerHTML = "testing1";
+    const spriteCheckbox2 = document.createElement("input");
+    spriteCheckbox2.setAttribute("type", "checkbox", "name", "sprites", "value", "back", "id", "back");
+    const spriteCheckbox2Label = document.createElement("label");
+    spriteCheckbox2Label.for = "back";
+    spriteCheckbox2Label.innerHTML = "testing2"
+    spriteFilterSection.innerHTML = "Show only: ";
+    
+    
+    const spriteDisplay = document.createElement("div");
+    spriteDisplay.setAttribute("style", "display: flex; gap: 1em; justify-content: space-between; width: 100%; overflow-x: scroll; border: solid 2px black;");
+    const gensArray = [];
+    const sprites = Object.entries(singlePoke.sprites.versions);
+        for (let i = 0; i < sprites.length; i++) {
+            const genGameList = Object.entries(sprites[i][1]);
+            // console.log("genGameList :>> ", genGameList)
+            for (let j = 0; j < genGameList.length; j++) {
+                const genGameCollection = genGameList[i][1];
+                for (let k = 0; k < genGameCollection.length; k++) {
+                    console.log("genGameCollection :>> ", genGameCollection)
+                    // const spriteImg  = document.createElement("img");
+                    // spriteImg.setAttribute("src", genGameCollection[i])
+                }
+            }
+        }
+    
+    
+    
+    spriteDisplay.innerHTML = 
 
 
-    modalContent1.append(mainImg, content1Right);
+
+    spriteFilterSection.append(spriteCheckbox1, spriteCheckbox1Label, spriteCheckbox2, spriteCheckbox2Label);
+    spriteHeader.append(spriteH3, spriteFilterSection, spriteDisplay)
+    spriteSection.append(spriteHeader);
+
+
+
+    abilitiesLine.append(abilitiesH3, abilitiesList)
+    weightSpan.append(weightH3, weight);
+    heightSpan.append(heightH3, height),
+    heightWeightLine.append(heightSpan, weightSpan)
+    typeLine.append(typesH3, types);
+    baseStatsLine.append(baseStatsH3, baseStatsTable);
+    content1Right.append(typeLine, baseStatsLine, heightWeightLine, abilitiesLine);
+    imgDiv.appendChild(mainImg);
+    modalContent1.append(imgDiv, content1Right);
     modalHeader.append(close, h1);
-    modal.append(modalHeader, modalContent1);
+    modal.append(modalHeader, modalContent1, spriteSection);
     modalBackground.appendChild(modal);
     body.appendChild(modalBackground);
-
-
 
     cardDiv.addEventListener("click", () => {
         modalBackground.style.display = "block";
